77421c9345ce50a9276befc5f54ccabc0a5c9f22
KyanoAmadeo <kyano_zuccheri@sluz.ch_>
2025-09-08 17:15:54 +0200

    docs: add Name

--- Program.cs
+++ Program.cs
@@ -2,9 +2,7 @@
 Console.WriteLine();
 Console.WriteLine("Mit diesem Programm üben wir die Zusammenarbeit auf GitHub.");
 Console.WriteLine("Gabor Karolyi");
-<<<<<<< HEAD
 Console.WriteLine("Tymofii Babchuk");
-=======
 Console.WriteLine("Leandro Sabotic");
->>>>>>> 9728b74d4ee1b79c58897912d8da4ae8b4a2f167
 Console.WriteLine("Kyano Zuccheri");
+Console.WriteLine("Cristian Torre de Francisco");
