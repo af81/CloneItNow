3c924973fff8d10d7205bab992d14dc51defe166
CristianTorre <cristian.torre@kauz.ch>
2025-09-08 16:35:05 +0200

    docs: add Cristian Torre de Francisco to team list

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hallo Lernende*r!");
 Console.WriteLine();
 Console.WriteLine("Mit diesem Programm üben wir die Zusammenarbeit auf GitHub.");
+Console.WriteLine("Cristian Torre de Francisco");
